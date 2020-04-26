ed650e2582bf55672d3214a09d5be84271919577
Hubert-Kujawiak <h.kujawiak@hotmail.com>
2020-04-26 13:05:56 +0200

    Projekt

--- src/js/LandingPage.js
+++ src/js/LandingPage.js
@@ -10,7 +10,7 @@ export default function LandingPage() {
                     <h1>Witaj w aplikacji serwisowej!</h1>
                     <p>Dodawaj samochód do bazy lub je wyszukuj.</p>
                     <p>Wszystkie naprawy oraz serwisy w jednym miejscu!</p>
-                    <button className="buttonLandPageFirst"><a href="http://localhost:3001/#/add">Dodaj Pojazd</a></button>
+                    <button className="buttonLandPageFirst"><Link to="/add">Dodaj Pojazd</Link></button>
                     <button className="buttonLandPageSecond"><Link to="/search">Baza Pojazdów</Link></button>
                 </div>
             </div>
