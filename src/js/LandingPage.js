51c713c7d905eea1407112d27c02b894e1edd971
Hubert-Kujawiak <h.kujawiak@hotmail.com>
2020-04-26 13:02:33 +0200

    Projekt

--- src/js/LandingPage.js
+++ src/js/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 export default function LandingPage() {
 
@@ -10,7 +11,7 @@ export default function LandingPage() {
                     <p>Dodawaj samochód do bazy lub je wyszukuj.</p>
                     <p>Wszystkie naprawy oraz serwisy w jednym miejscu!</p>
                     <button className="buttonLandPageFirst"><a href="http://localhost:3001/#/add">Dodaj Pojazd</a></button>
-                    <button className="buttonLandPageSecond"><a href="http://localhost:3001/#/search">Baza Pojazdów</a></button>
+                    <button className="buttonLandPageSecond"><Link to="/search">Baza Pojazdów</Link></button>
                 </div>
             </div>
         </>
